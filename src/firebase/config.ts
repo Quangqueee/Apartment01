4b4c0b3fc5cceb854ce3463194a83cbfa2287785
Quang Nguyen Duc <kodauten1@gmail.com>
2025-11-13 02:44:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3489924431-24ff3",
+  "appId": "1:959481429323:web:a0312e7520941d1e6b3cce",
+  "apiKey": "AIzaSyDUGIFiKeD5SODQYmzPaEUQRbSLF7xXcJs",
+  "authDomain": "studio-3489924431-24ff3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "959481429323"
+};
