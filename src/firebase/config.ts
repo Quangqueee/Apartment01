3fdebe07c01929921e7a33b78552c8f06db27aab
Quang Nguyen Duc <kodauten1@gmail.com>
2025-11-13 06:39:50 +0000

    Như vậy đã được chưa, gs://quang-apartment.firebasestorage.app tôi thấy

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAp99Jd23i86FM2adUEKAa4c5PVWP8X5QM",
   authDomain: "quang-apartment.firebaseapp.com",
   projectId: "quang-apartment",
-  storageBucket: "quang-apartment.appspot.com",
+  storageBucket: "quang-apartment.firebasestorage.app",
   messagingSenderId: "305244701927",
   appId: "1:305244701927:web:3f49a89d1726f7d4680e42",
   measurementId: "G-4LKV4KFZW9"
