7ce9e4bf85eaba01e5dc1a4a396761bd634b3d4d
Quang Nguyen Duc <kodauten1@gmail.com>
2025-11-13 05:39:53 +0000

    GET /?district=T%C3%A2y+H%E1%BB%93&page=1 200 in 372ms  ○ Compiling /adm

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:959481429323:web:a0312e7520941d1e6b3cce",
   "apiKey": "AIzaSyDUGIFiKeD5SODQYmzPaEUQRbSLF7xXcJs",
   "authDomain": "studio-3489924431-24ff3.firebaseapp.com",
+  "storageBucket": "studio-3489924431-24ff3.appspot.com",
   "measurementId": "",
   "messagingSenderId": "959481429323"
 };
